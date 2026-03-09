124182120273ecb4fd632b9faed38e79345c4b59
Jinchenko <56926910+jindukwentua@users.noreply.github.com>
2026-03-09 13:13:50 +0100

    new changes

--- src/app/api/latest-post/route.ts
+++ src/app/api/latest-post/route.ts
@@ -2,6 +2,8 @@ import fs from "fs";
 import path from "path";
 import { NextResponse } from "next/server";
 
+export const runtime = "nodejs";
+
 const postsDirectory = path.join(process.cwd(), "src/app/blog");
 
 type PostMetadata = {
